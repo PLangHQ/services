9a41a63c254d57cddfc22232f52c3032840207ce
Ingi Gauti <ingigauti@gmail.com>
2024-02-16 09:04:44 +0000

    remove .db and add to ignore

--- PLang.AskUserMessage/AskUserMessageHandler.cs
+++ PLang.AskUserMessage/AskUserMessageHandler.cs
@@ -28,6 +28,8 @@ namespace PLang.AskUserMessage
 			Dictionary<string, object?> parameters = [];
 			parameters.Add("content", ex.Message);
 			parameters.Add("to", adminAddress);
+			
+			InstallServiceGoal();
 
 			await pseudoRuntime.RunGoal(engine, context, Path.DirectorySeparatorChar.ToString(), ".services/AskUserMessage/SendMessage", parameters);
 
@@ -39,6 +41,8 @@ namespace PLang.AskUserMessage
 				{
 					// the exception contain link to method that should be called when user answers
 					// using InvokeCallback, it will call the method and send in the answer.
+					// checkout https://github.com/PLangHQ/plang/blob/main/PLang/Exceptions/AskUser/Database/AskUserDatabaseType.cs
+					// to see how the InvokeCallback uses LLM to map the answer from the user to a class.
 					await ex.InvokeCallback(answer);
 					memoryStack.Put("AskUserResponse", null);
 					return true;
@@ -47,6 +51,11 @@ namespace PLang.AskUserMessage
 				await Task.Delay(1000);
 			}
 
+		}
+
+		private void InstallServiceGoal()
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
